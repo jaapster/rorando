505640062e3b03c061d3fba9c3b16615f08d85af
Jaap <jaaplous@gmail.com>
2023-09-28 09:41:37 +0200

    lfo stuff

--- synth.c
+++ synth.c
@@ -8,10 +8,10 @@
 #define PIN_CH_MUX_C 4
 #define PIN_CS_MUX_0 5
 #define PIN_CS_MUX_1 6
+#define LED_PIN PICO_DEFAULT_LED_PIN
+#define BLINK_TIME 100000
 
 #define NUM_MUX 2
-
-#define NUM_DCO 4
 
 // function declarations
 void cv_task();
@@ -21,16 +21,24 @@ void dac_task();
 void init_pin(uint8_t pin);
 void led_init();
 void led_task();
+void lfo_init();
+void lfo_task();
 void mux_init();
 
 // variables
-const uint8_t mux_cs[2] = {PIN_CS_MUX_0, PIN_CS_MUX_1};
+const uint8_t mux_cs[NUM_MUX] = {PIN_CS_MUX_0, PIN_CS_MUX_1};
 const uint8_t mux_bit[3] = {PIN_CH_MUX_A, PIN_CH_MUX_B, PIN_CH_MUX_C};
 
 uint32_t led_start = 0;
-uint16_t value = 0;
 uint8_t cv_cursor = 0;
 uint16_t cv[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+// patch parameters
+uint32_t lfo_lvl = 0;
+// uint16_t lfo_hz = 16;
+uint32_t lfo_max = 131072 / 16; // 131072 is about 1 Hz
+uint8_t lfo_inc = 3;
+uint32_t lfo_tick = 0;
 
 //
 int main() {
@@ -40,6 +48,7 @@ int main() {
     mux_init();
 
     while (1) {
+        lfo_task();
         cv_task();
         dac_task();
         led_task();
@@ -63,26 +72,36 @@ void dac_set(uint16_t value) {
 
 // test function that outputs a saw wave
 void dac_task() {
-    if (value > 4095) {
-        value = 0;
-    }
+    uint32_t f = 524288 / lfo_max;
+    uint32_t h = lfo_lvl * f;
+    uint16_t v = h / 128;
 
-    value += 16;
-
-    dac_set(value);
+    dac_set(v > 4095 ? 4095 : v < 0 ? 0 : v);
 }
 
 void led_init() {
-    init_pin(PICO_DEFAULT_LED_PIN);
+    init_pin(LED_PIN);
     led_start = time_us_32();
 }
 
 void led_task() {
-    gpio_put(PICO_DEFAULT_LED_PIN, time_us_32() - led_start < 100000 ? 1 : 0);
+    gpio_put(LED_PIN, time_us_32() - led_start < BLINK_TIME ? 1 : 0);
+}
+
+void lfo_init() {
+    lfo_tick = time_us_32();
+}
+
+void lfo_task() {
+    lfo_lvl += lfo_inc - 2;
+    lfo_inc = lfo_lvl == 0 ? 3 : lfo_lvl == lfo_max ? 1 : lfo_inc;
 }
 
 void mux_init() {
+    // initialize chip select pins
     for (int i = 0; i < NUM_MUX; i++) init_pin(mux_cs[i]);
+
+    // initialize channel select pins
     for (int i = 0; i < 3; i++) init_pin(mux_bit[i]);
 }
 
