1245d75392984b8cd650b2360ead846b74b59418
Jaap Lous <jaaplous@gmail.com>
2023-09-29 16:12:15 +0200

    merged logic from earlier effort

--- synth.c
+++ synth.c
@@ -1,10 +1,17 @@
-//
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include "hardware/pio.h"
+#include "hardware/uart.h"
+#include "hardware/clocks.h"
+#include "synth.pio.h"
 
+#define BAUD_RATE 31250
+#define UART_ID uart0
+#define PIN_UART_TX 0
+#define PIN_UART_RX 1
 #define PIN_CH_MUX_A 2
 #define PIN_CH_MUX_B 3
 #define PIN_CH_MUX_C 4
@@ -13,6 +20,7 @@
 #define LED_PIN PICO_DEFAULT_LED_PIN
 #define BLINK_TIME 100000
 
+#define MIDI_CHANNEL 1
 #define NUM_MUX 2
 #define NUM_DCO 4
 #define NUM_VOICE 2
@@ -34,6 +42,8 @@ void dac_init();
 void dac_set(uint16_t value);
 void dac_task();
 void env_task();
+uint8_t get_free_voice();
+float get_freq_from_midi_note(uint8_t note);
 uint32_t hz_to_us(float hz);
 void init_pin(uint8_t pin);
 void led_init();
@@ -42,11 +52,22 @@ void lfo_task();
 void midi_init();
 void midi_task();
 void mux_init();
+void note_off(uint8_t note);
+void note_on(uint8_t note, uint8_t velocity);
+void pio_init();
+void set_amplitude(uint8_t dco, float freq);
+void set_frequency(uint8_t dco, float freq);
 void trigger_envelope(uint8_t voice);
+void voices_task();
 
 // constants
+const uint8_t reset_pins[NUM_DCO] = {12, 13, 14, 15};
+const uint8_t dco_to_pio[NUM_DCO] = {0, 0, 0, 0};
+const uint8_t dco_to_sm[NUM_DCO] = {0, 1, 2, 3};
 const uint8_t mux_cs[NUM_MUX] = {PIN_CS_MUX_0, PIN_CS_MUX_1};
 const uint8_t mux_bit[3] = {PIN_CH_MUX_A, PIN_CH_MUX_B, PIN_CH_MUX_C};
+const PIO pio[2] = {pio0, pio1};
+const float BASE_NOTE = 440.0f;
 
 // system variables
 uint8_t cv_cursor = 0;
@@ -60,6 +81,11 @@ uint32_t lfo_delay_tick = 0;
 uint16_t lfo_lvl = 0;
 uint32_t lfo_tick = 0;
 uint16_t lfo_val = 0;
+uint8_t midi_serial_status = 0;
+uint8_t next_voice = 0;
+uint32_t VOICE_TIMES[NUM_VOICE] = {0, 0};
+uint8_t VOICE_NOTES[NUM_VOICE] = {0, 0};
+uint8_t VOICE_GATES[NUM_VOICE] = {0, 0};
 
 // patch variables
 uint16_t env_a = 200; // time in ms
@@ -69,6 +95,19 @@ uint16_t env_r = 1000; // time in ms
 float lfo_freq = 5; // hz
 uint8_t lfo_wav = SAW;
 uint32_t lfo_delay = 0;
+uint8_t range_1 = 0; // allowed: 0, 1, 2
+uint8_t range_2 = 0; // allowed: 0, 1, 2
+uint8_t tune_2 = 128; // +/- 1200 cent (12 semi-tones)
+uint8_t fine_2 = 128; // +/- 50 cent (half a semi-tone)
+uint8_t vcf_cutoff = 0;
+uint8_t vcf_reso = 0;
+uint8_t lfo_1 = 128;
+uint8_t lfo_2 = 128;
+uint8_t lfo_vcf = 0;
+uint8_t env_1 = 0;
+uint8_t env_2 = 0;
+uint8_t env_vcf = 0;
+uint8_t env_vca = 0;
 
 //
 int main() {
@@ -77,6 +116,7 @@ int main() {
     led_init();
     midi_init();
     mux_init();
+    pio_init();
 
     trigger_envelope(0);
 
@@ -86,6 +126,7 @@ int main() {
         lfo_task();
         cv_task();
         dac_task();
+        voices_task();
         led_task();
     }
 }
@@ -182,6 +223,35 @@ void env_task() {
     }
 }
 
+uint8_t get_free_voice() {
+    uint32_t oldest_time = time_us_32();
+    uint8_t oldest_voice = 0;
+
+    // Here we round robin the voices so each note will be played on the next
+    // available (gate == 0) voice. If no voice is available, the voice that
+    // was triggered the longest ago will be picked.
+    for (int i = 0; i < NUM_VOICE; i++) {
+        uint8_t n = (next_voice + i) % NUM_VOICE;
+
+        if (VOICE_GATES[n] == 0) {
+            next_voice = (n + 1) % NUM_VOICE;
+            return n;
+        }
+
+        if (VOICE_TIMES[i] < oldest_time) {
+            oldest_time = VOICE_TIMES[i];
+            oldest_voice = i;
+        }
+    }
+
+    next_voice = (oldest_voice + 1) % NUM_VOICE;
+    return oldest_voice;
+}
+
+float get_freq_from_midi_note(uint8_t note) {
+    return pow(2, (note - 69) / 12.0f) * BASE_NOTE;
+}
+
 // hz to microseconds
 uint32_t hz_to_us(float hz) {
     return 1000000 / hz;
@@ -240,11 +310,60 @@ void lfo_task() {
 }
 
 void midi_init() {
+	uart_init(UART_ID, BAUD_RATE);
 
+	gpio_set_function(PIN_UART_TX, GPIO_FUNC_UART);
+	gpio_set_function(PIN_UART_RX, GPIO_FUNC_UART);
+
+	uart_set_fifo_enabled(UART_ID, true);
+
+	// disable cr/lf conversion on Tx
+	uart_set_translate_crlf(UART_ID, false);
 }
 
 void midi_task() {
+    if (!uart_is_readable(UART_ID)) return;
 
+    uint8_t lsb = 0;
+    uint8_t msb = 0;
+    uint8_t data = uart_getc(UART_ID);
+
+    led_tick = time_us_32();
+
+    // status
+    if (data >= 0xF0 && data <= 0xF7) {
+        midi_serial_status = 0;
+        return;
+    }
+
+    // realtime message
+    if (data >= 0xF8 && data <= 0xFF) {
+        return;
+    }
+
+    if (data >= 0x80 && data <= 0xEF) {
+        midi_serial_status = data;
+    }
+
+    if (midi_serial_status >= 0x80 && midi_serial_status <= 0x9F ||
+        midi_serial_status >= 0xB0 && midi_serial_status <= 0xBF || // cc mess
+        midi_serial_status >= 0xE0 && midi_serial_status <= 0xEF) {
+
+        lsb = uart_getc(UART_ID);
+        msb = uart_getc(UART_ID);
+    }
+
+    if (midi_serial_status == (0x90 | (MIDI_CHANNEL - 1))) {
+        if (msb > 0) {
+            note_on(lsb, msb);
+        } else {
+            note_off(lsb);
+        }
+    }
+
+    if (midi_serial_status == (0x80 | (MIDI_CHANNEL - 1))) {
+        note_off(lsb);
+    }
 }
 
 void mux_init() {
@@ -255,6 +374,90 @@ void mux_init() {
     for (uint8_t i = 0; i < 3; i++) init_pin(mux_bit[i]);
 }
 
+void note_off(uint8_t note) {
+    for (int i = 0; i < NUM_VOICE; i++) {
+        if (VOICE_NOTES[i] == note) {
+            VOICE_TIMES[i] = 0;
+            VOICE_GATES[i] = 0;
+
+            // test
+            VOICE_NOTES[i] = 0;
+        }
+    }
+}
+
+void note_on(uint8_t note, uint8_t velocity) {
+    for (int i = 0; i < NUM_VOICE; i++) {
+        if (VOICE_NOTES[i] == note) return;
+    }
+
+    uint8_t voice_num = get_free_voice();
+
+    VOICE_TIMES[voice_num] = time_us_32();
+    VOICE_NOTES[voice_num] = note;
+    VOICE_GATES[voice_num] = 1;
+}
+
+void pio_init() {
+    // pio init
+    uint offset[2];
+    offset[0] = pio_add_program(pio[0], &frequency_program);
+    offset[1] = pio_add_program(pio[1], &frequency_program);
+
+    for (int i = 0; i < NUM_DCO; i++) {
+        uint8_t p = dco_to_pio[i];
+        init_sm_pin(pio[p], dco_to_sm[i], offset[p], reset_pins[i]);
+        pio_sm_set_enabled(pio[p], dco_to_sm[i], true);
+    }
+}
+
+void set_amplitude(uint8_t dco, float freq) {
+    // this should result in a saw wave amplitude of 12 volts rail-tp-rail
+    // measured at U104 pin 7 and pin 1
+
+    // todo set level in cv array
+}
+
+void set_frequency(uint8_t dco, float freq) {
+    PIO p = pio[dco_to_pio[dco]];
+    uint sm = dco_to_sm[dco];
+
+    uint32_t clk_div = clock_get_hz(clk_sys) / 2 / freq;
+
+    if (freq == 0) clk_div = 0;
+
+    pio_sm_put(p, sm, clk_div);
+    pio_sm_exec(p, sm, pio_encode_pull(false, false));
+    pio_sm_exec(p, sm, pio_encode_out(pio_y, 32));
+
+    set_amplitude(dco, freq);
+}
+
 void trigger_envelope(uint8_t voice) {
     env_phase[voice] = ATTACK;
+}
+
+void voices_task() {
+    for (int i = 0; i < NUM_VOICE; i++) {
+        uint8_t note = VOICE_NOTES[i];
+        uint16_t env = env_lvl[i];
+
+        // update dco frequencies
+        uint8_t dco1 = i * 2;
+        uint8_t dco2 = dco1 + 1;
+
+        float freq = get_freq_from_midi_note(note);
+        // todo calculate boundaries
+        float tune_f = (tune_2 - 128) / 128;
+        float fine_f = (fine_2 - 128) / (128 * 24);
+        float lfo_1_f = lfo_lvl * (lfo_1 / 255) / 20000;
+        float lfo_2_f = lfo_lvl * (lfo_2 / 255) / 20000;
+        float env_1_f = env * (env_1 / 255);
+        float env_2_f = env * (env_2 / 255);
+        float f_1 = range_1 + lfo_1_f + env_1_f;
+        float f_2 = range_2 + lfo_2_f + env_2_f + tune_f + fine_f;
+
+        set_frequency(dco1, freq * pow(2, f_1));
+        set_frequency(dco2, freq * pow(2, f_2));
+    }
 }
